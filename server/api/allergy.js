020d2195371c4b1710ee12f87e4d8023a60aec75
Chris Chin <chin.christopher@gmail.com>
2019-12-03 12:27:00 -0500

    add allergy routes

--- server/api/allergy.js
+++ server/api/allergy.js
@@ -1,4 +1,39 @@
-// const router = require('express').Router()
-// const {Fridge, FridgeStock, User, Item} = require('../db/models')
-// const Sequelize = require('sequelize')
-// module.exports = router
+const router = require('express').Router()
+const {Fridge, FridgeStock, User, Item, Allergy} = require('../db/models')
+const Sequelize = require('sequelize')
+module.exports = router
+
+// GET a specific user's allergies
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId)
+    const allergies = await user.getAllergies()
+    res.send(allergies)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// POST a new allergy to a specific user
+router.post('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId)
+    const newAllergy = await user.addAllergy(req.body.allergyId)
+    // console.log(newAllergy)
+    res.send(newAllergy)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId)
+    const deletedAllergy = {allergyId: req.body.allergyId}
+    await user.removeAllergy(req.body.allergyId)
+    console.log('deletedAllergy ===', deletedAllergy)
+    res.send(deletedAllergy)
+  } catch (error) {
+    next(error)
+  }
+})
--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,7 @@ router.use('/users', require('./users'))
 router.use('/fridge', require('./fridge'))
 router.use('/recipes', require('./recipes'))
 router.use('/item', require('./item'))
+router.use('/allergy', require('./allergy'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
